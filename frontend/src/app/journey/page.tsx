f1cd68c415a0c2511ad5e59fd26ee5d3a8757c49
Chris <c.k.onderhoudsbedrijf@gmail.com>
2025-07-27 09:15:22 +0200

    FIXED LOOPS: Removed wallet connect requirement from Trading Journey & Tiger RISK

--- frontend/src/app/journey/page.tsx
+++ frontend/src/app/journey/page.tsx
@@ -1,12 +1,9 @@
 'use client'
 
 import TradingJourney from '@/components/dashboard/TradingJourney'
-import ProtectedRoute from '@/components/ProtectedRoute'
 
 export default function JourneyPage() {
   return (
-    <ProtectedRoute requireVerification={true}>
-      <TradingJourney />
-    </ProtectedRoute>
+    <TradingJourney />
   )
 } 
--- frontend/src/app/risk/page.tsx
+++ frontend/src/app/risk/page.tsx
@@ -1,12 +1,9 @@
 'use client'
 
 import TigerRisk from '@/components/risk/TigerRisk'
-import ProtectedRoute from '@/components/ProtectedRoute'
 
 export default function RiskPage() {
   return (
-    <ProtectedRoute requireVerification={true}>
-      <TigerRisk />
-    </ProtectedRoute>
+    <TigerRisk />
   )
 } 
